ffbc0ad89e3e47d7825dc2b66bcdf086767f3521
javiperlo <econectmail@gmail.com>
2023-10-11 20:21:44 +0200

    Modificado getnextline

--- get_next_line/variablesEstaticas.c
+++ get_next_line/variablesEstaticas.c
@@ -1,51 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   variablesEstaticas.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: javperez <javperez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/06 11:24:54 by javperez          #+#    #+#             */
-/*   Updated: 2023/10/11 19:54:01 by javperez         ###   ########.fr       */
+/*   Created: 2023/10/11 18:09:25 by javperez          #+#    #+#             */
+/*   Updated: 2023/10/11 20:21:02 by javperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
-
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 0
-#endif
-
-char	*get_next_line(int fd)
-{
-	char buf[BUFFER_SIZE];
-	ssize_t nr_bytes;
-
-	nr_bytes = 0;
-	if (fd == -1)
-		return (NULL);
-	else
-	{
-		nr_bytes = read(fd, buf, BUFFER_SIZE);
-		close(fd);
-		if (nr_bytes == 0)
-			return (NULL);
-		else
-		{
-			printf("%s\n", buf);
-		}
-	}
-	return (0);
-}
-
-int	main(void)
+#include <unistd.h>
+int main()
 {
 	int	fd;
-
-	fd = open("miArchivoCorto", O_RDONLY);
+	char buf[256];
+	int nr_bytes;
 	
-	get_next_line(fd);
+	nr_bytes = 0;
+	printf("Nr_bytes --> %d\n", nr_bytes);
+	fd = open("miArchivo", O_RDONLY);
+	while ((nr_bytes = read(fd, buf, 5)))
+	{
+		buf[nr_bytes] = '\0';
+		printf("buf --> %s\n", buf);
+	}
+	close(fd);
 }
