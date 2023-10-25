62b5a1b53f846df8b23afcc2e27ca26eaf3319ae
javiperlo <econectmail@gmail.com>
2023-10-25 13:33:13 +0200

    get_next_line Comentado

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -5,78 +5,173 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: javperez <javperez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/06 11:24:54 by javperez          #+#    #+#             */
-/*   Updated: 2023/10/21 14:18:15 by javperez         ###   ########.fr       */
+/*   Created: 2023/10/24 18:54:01 by javperez          #+#    #+#             */
+/*   Updated: 2023/10/25 13:29:52 by javperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+/**
+ * Esta función splitea la string devuelta de read_line() y solo
+ * te saca hasta el salto de linea.
+*/
+
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
-#include "../libft/libft.h"
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 10
-#endif
-
-char *split_and_join_words(int fd);
-
-int check_next_line(char *buff)
+char	*get_line(char *str)
 {
-	//int 	len;
 	int		i;
-	i = 0;
-	while (buff[i] != '\0')
-	{
-		if(buff[i] == '\n')
-			return (1);
-		i ++;
-	}
-	return (0);
-}
-char *split_and_join_words(int fd)
-{
-	ssize_t nr_bytes;
-	char buff[BUFFER_SIZE];
-	int i = 0;
-	char *new_block;
-	char *line;
+	char	*cut_str;
 
-	if (fd == -1)
+	i = 0;
+	if (!str[i])
 		return (NULL);
-	nr_bytes = read(fd, buff, BUFFER_SIZE);
-	//printf("Se van a imprimir %ld bytes, y el buffer es: \n%s\n", nr_bytes,buff);
-	new_block = ft_substr(buff, i, i + nr_bytes); //Cogemos en cada iteración, desde i hasta i + nr_bytes
-	line = (char *)malloc((ft_strlen(new_block) * sizeof(char)) + 1); // Reserva espacio en memoria para cada grupo de palabras
-	if (!line)
+	while (str[i] && str[i] != '\n')
+		i++;
+	cut_str = (char *)malloc(sizeof(char) * (i + 2));
+	if (!cut_str)
 		return (NULL);
-	i += nr_bytes; //E iteramos de nuevo para quedarnos con el siguiente trozo de BUFFER_SIZE
-	check_next_line(new_block);
-	return (line); 
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		cut_str[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+	{
+		cut_str[i] = str[i];
+		i++;
+	}
+	cut_str[i] = '\0';
+	return (cut_str);
+}
+
+/**
+ * Esta función está retornando el valor sobrante de la linea en bruto
+ * después del salto de línea.
+*/
+
+char	*new_line(char *left_str)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (left_str[i] && left_str[i] != '\n')
+		i++;
+	if (!left_str[i])
+	{
+		free(left_str);
+		return (NULL);
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(left_str) - i + 1));
+	if (!str)
+		return (NULL);
+	i++; //Saltamos el \n
+	j = 0;
+	while (left_str[i]) //Y copiamos el resto de la string en str con la iteracion de j.
+		str[j++] = left_str[i++];
+	str[j] = '\0';
+	free(left_str);
+	return (str);
+}
+
+/*Esta función te pilla todo el texto almacenado en BUFFER_SIZE*/
+
+char	*read_line(int fd, char *str)
+{
+	char	*buff;
+	int		rd_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(str, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[rd_bytes] = '\0';
+		str = ft_strjoin(str, buff);
+	}
+	free(buff);
+	return (str);
+}
+/*		GET_NEXT_LINE
+		-------------
+	Esta función va a devolver la línea a leer.
+	Si se llama múltiples veces a la función,
+	la función "memorizará" la posición anterior
+	y leerá la siguiente.
+
+	Declaramos una variable "line" -> Que va a ser
+	la string que vamos a retornar.
+	
+	También declaramos una variable estática "str" 
+	que se encargará de conservar la posición en la que
+	se quedó anteriormente.
+
+	En primer lugar verificamos que el diccionario a leer "fd"
+	contenga algo. fd va a devolver un número mayor que 0 si 
+	contiene contenido. Si no contiene devolverá -1. Por lo tanto,
+	verificamos que si es < 1 y BUFFER_SIZE <= a 0, retorna NULL.
+
+	A continuación asignamos a la variable estática, el valor bruto
+	que se haya obtenido de la función read_line(fd).
+	Verificamos que "str" contenga algo y de lo contrario, devolvemos
+	NULL.
+
+	Ahora almacenamos en la variable "line", **SOLO** hasta el \n
+	para poder retornar el valor.
+
+	Por último tenemos que actualizar la variable estática "str".
+	Para ello utilizamos la función new_line(str), a la que le pasamos por
+	argumento en la primera iteración el valor BRUTO obtenido de la función
+	read_line. 
+
+	Esta función va a iterar sobre la cadena y va a retornar el valor que hay
+	después del salto de línea (\n).
+
+	Entonces, cuando se vuelva a llamar a la función, "str", ya tendrá un valor
+	estático. Que se añadirá con un ft_strjoin() al valor de str.
+*/
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	str = read_line(fd, str); //Read line devuelve la linea en bruto.
+	if (!str)
+		return (NULL);
+	line = get_line(str);
+	str = new_line(str);
+	return (line);
 }
 
 int	main(void)
 {
-	int	fd;
-	int	i;
-	char *line;
+	char	*line;
+	int		i;
+	int		fd1;
 
-	i = 0;
-	fd = open("txt_files/miArchivoCorto", O_RDONLY);
-	while (i < 1) //Implementar un while para que lea las lineas que queramos, en este caso: 1
+	fd1 = open("txt_files/file", O_RDONLY); /* Abrimos el diccionario*/
+	i = 1;
+	while (i < 2) //Aquí ponemos las lienas que queramos leer
 	{
-		line = split_and_join_words(fd);
-		printf("Linea %d: %s\n", i, line);
+		line = get_next_line(fd1); //Almacenamos en "line", la variable almacenada de get_next_line(fd);
+		printf("line [%02d]: %s", i, line); // Y la printeamos
+		free(line); //Liberamos la memoria de "line", para poder almacenar otra cadena distinta.
 		i++;
 	}
-	
-	/**
-	 * PRUEBASS
-	*/
-	/*
-	char *line;
-	line = "Hola";
-	printf("%d\n", check_next_line(line));
-	*/
+	close(fd1); /*Una vez hayamos terminado de leer las lineas que queramos, cerramos el diccionario*/
+	return (0);
 }
---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javperez <javperez@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 18:16:30 by javperez          #+#    #+#             */
+/*   Updated: 2023/10/25 13:28:00 by javperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
+char	*read_line(int fd, char *str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+int		ft_strlen(const char *str);
+char	*get_line(char *left_str);
+char	*new_line(char *left_str);
+
+#endif
